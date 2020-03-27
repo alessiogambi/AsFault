42b54590cc4a7d8c2224deb29de034c34deb95cd
Alessio Gambi <alessio.gambi@uni-passau.de>
2020-03-27 23:11:42 +0100

    Made the scripts configurable

--- scripts/create_experiments.ps1
+++ scripts/create_experiments.ps1
@@ -1,11 +1,26 @@
 Param (
+    [Parameter(Mandatory=$true)]$envDir,
+    [Parameter(Mandatory=$true)]$experimentDir,
     [Parameter(Mandatory=$true)]$start,
     [Parameter(Mandatory=$true)]$end
 )
 
+# $envDir=".esec-fse-20-beamng-small-env"
+# $experimentDir="./FSE20-beamng-small"
+
+# $envDir=".esec-fse-20-beamng-large-env"
+# $experimentDir="./FSE20-beamng-large"
+
+# $envDir=".esec-fse-20-beamng-small-env-with-repair"
+# $experimentDir="./FSE20-beamng-small-with-repair"
+
+#$envDir=".esec-fse-20-beamng-large-env-with-repair"
+#$experimentDir="./FSE20-beamng-large-with-repair"
+
+
 $start..$end |
    Foreach-Object {
-	$exps_dir = "./FSE20/$_"
+	$exps_dir = "$experimentDir/$_"
 	New-Item -ItemType "directory" -Path "$exps_dir"
-	Copy-Item ".esec-fse-20-beamng-small-env" -Destination "$exps_dir/.esec-fse-20-beamng-small-env" -Recurse
+	Copy-Item $envDir -Destination "$exps_dir/$envDir" -Recurse
 }
--- scripts/run_experiments.ps1
+++ scripts/run_experiments.ps1
@@ -1,17 +1,39 @@
-# List all the experiment folders inside ./experiments
+Param (
+    [Parameter(Mandatory=$true)]$envDir,
+    [Parameter(Mandatory=$true)]$experimentsDir,
+    [Parameter(Mandatory=$true)]$timeLimit
+)
+
+# $envDir=".esec-fse-20-beamng-small-env"
+# $experimentsDir="./FSE20-beamng-small"
+
+# $envDir=".esec-fse-20-beamng-small-env-with-repair"
+# $experimentsDir="./FSE20-beamng-small-with-repair"
+
+# $envDir=".esec-fse-20-beamng-large-env"
+# $experimentsDir="./FSE20-beamng-large"
+
+# $envDir=".esec-fse-20-beamng-large-env-with-repair"
+# $experimentsDir="./FSE20-beamng-large-with-repair"
+
 $generations=100
 
-# Stop the simulation after 1h
-$time_limit=3600
-
-dir ./FSE20 -Directory |
+dir $experimentsDir -Directory |
 Foreach-Object {
 	$experiment = $_.FullName
-    Write-Host  "$($experiment)"
-	$log = "$($experiment)\experiment.log"
-    $env = "$($experiment)\.esec-fse-20-beamng-small-env"
-    # Execute the experiment from the right folder
-    cd C:\Users\Alessio\AsFault
-    C:\Users\Alessio\AsFault\.alessio\Scripts\python.exe .\src\asfault\app.py --log $log evolve --env $env bng --generations $generations --time-limit $time_limit --render
-    cd C:\Users\Alessio\AsFault\scripts
+
+    # Check if this experiments already ran, hence produced a log file
+    $log = "$($experiment)\experiment.log"
+    
+    if (Test-Path -Path $log -PathType Leaf){
+        "Experiment $experiment already run. Skip it"
+    } else {
+        "Executing experiment $experiment"
+        $env = "$($experiment)\$envDir"
+
+        # Execute the experiment from the right folder
+        cd C:\Users\Alessio\AsFault
+        C:\Users\Alessio\AsFault\.alessio\Scripts\python.exe .\src\asfault\app.py --log $log evolve --env $env bng --generations $generations --time-limit $timeLimit --render
+        cd C:\Users\Alessio\AsFault\scripts
+    }
 }   
