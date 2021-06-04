d169d2dc7d7a36f53821ab05a5edfefaeee2a774
zohdit <zohdit@usi.ch>
2021-06-04 12:32:50 +0200

    command to run

--- scripts/run_nvidia_experiments.ps1
+++ scripts/run_nvidia_experiments.ps1
@@ -36,7 +36,7 @@ Foreach-Object {
             
             # Execute the experiment from the right folder
             #cd C:\Users\Alessio\AsFault\
-            python.exe C:\modified-asfault\AsFault\src\asfault\app.py --log $log evolve --env $environmentDir ext --generations $generations --time-limit $timeLimit --use-simulation-time --render BeamNG
+            python.exe D:\tara\AsFault\src\asfault\app.py --log $log evolve --env $environmentDir ext --generations $generations --time-limit $timeLimit --use-simulation-time --render BeamNG
             #$wrapperScript
             #cd C:\Users\Alessio\AsFault\scripts
         }
